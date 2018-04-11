5f5c15eb14b52a50b256bb7069ecec0b7224e89a
Osniel L Teixeira <niellt@hotmail.com>
2018-04-11 14:06:50 -0300

    Algumas alterações

---
+++ Keynote_WTT/RefeicaoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RefeicaoTableViewCell.swift
+//  Keynote_WTT
+//
+//  Created by Osniel Lopes Teixeira on 11/04/2018.
+//  Copyright © 2018 Osniel Lopes Teixeira. All rights reserved.
+//
+
+import UIKit
+
+class RefeicaoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
