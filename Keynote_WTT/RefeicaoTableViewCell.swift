15fed200c32e5f29e5b7750b4869ec47e7a3a850
Osniel L Teixeira <niellt@hotmail.com>
2018-04-11 15:27:58 -0300

    Implementa a RefeicaoTableViewCell e a RefeicaoTableViewController

--- Keynote_WTT/RefeicaoTableViewCell.swift
+++ Keynote_WTT/RefeicaoTableViewCell.swift
@@ -9,16 +9,23 @@
 import UIKit
 
 class RefeicaoTableViewCell: UITableViewCell {
-
+    
+    //IBOutlets
+    @IBOutlet weak var refeicaoImage: UIImageView!
+    @IBOutlet weak var nomeLabel: UILabel!
+    @IBOutlet weak var horaLabel: UILabel!
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
 }
+
