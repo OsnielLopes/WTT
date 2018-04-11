15cb8746fd622698d2c736b66edd3b8fb51c6d29
Osniel L Teixeira <niellt@hotmail.com>
2018-04-11 15:29:21 -0300

    Opa

--- Keynote_WTT/ReifeicoesTableViewController.swift
+++ Keynote_WTT/ReifeicoesTableViewController.swift
@@ -10,28 +10,37 @@ import UIKit
 
 class ReifeicoesTableViewController: UITableViewController {
     
-    let refeicoes: [Refeicao]! = nil
+    let refeicoes: [Refeicao] = RefeicaoDataManager.shared.getAll()
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-//        refeicoes = RefeicaoData.getAll()
-    }
-
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return refeicoes.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell.init()
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "RefeicaoTableViewCell", for: indexPath) as? RefeicaoTableViewCell else {
+            fatalError("The dequeued cell is not an instance of RefeicaoTableViewCell.")
+        }
+        
+        let refeicao = refeicoes[indexPath.row]
+        
+        cell.nomeLabel.text = refeicao.nome
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .none
+        dateFormatter.timeStyle = .medium
+        cell.horaLabel.text = dateFormatter.string(from: refeicao.hora)
+        cell.refeicaoImage = UIImageView(image: refeicao.image)
+        
+        return cell
     }
-
-
+    
+    
 }
