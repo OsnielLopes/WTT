0e32e5cde0da4dadb2d49ad34ac4f9918262d83a
Guilherme Paciulli <guilherme.paciulli@icloud.com>
2018-04-11 15:06:45 -0300

    Fixing problems with git

--- Keynote_WTT/NovaRefeicaoViewController.swift
+++ Keynote_WTT/NovaRefeicaoViewController.swift
@@ -17,6 +17,9 @@ class NovaRefeicaoViewController: UIViewController {
     //Instance Variables
     var imagePicker: UIImagePickerController!
     
+    //New refeicao properties
+    var refeicaoImage: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.imagePicker = UIImagePickerController()
@@ -26,12 +29,23 @@ class NovaRefeicaoViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.textField.isEnabled = false
+    }
+    
+    var firstTime = true
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if firstTime { self.present(self.imagePicker, animated: true, completion: nil) }
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        self.firstTime = false
     }
     
     @IBAction func salvarRefeicao(_ sender: Any) {
-        if let name = self.textField.text {
-            let refeicao = Refeicao.init(id: nil, nome: name, hora: Date(), image: UIImage())
+        if let name = self.textField.text, let image = self.refeicaoImage {
+            let refeicao = Refeicao.init(id: nil, nome: name, hora: Date(), image: image)
             RefeicaoDataManager.shared.add(refeicao: refeicao)
         }
     }
@@ -39,13 +53,19 @@ class NovaRefeicaoViewController: UIViewController {
     @IBAction func scanFoodButtonPressed(_ sender: Any) {
         self.present(self.imagePicker, animated: true, completion: nil)
     }
+    
+    override func performSegue(withIdentifier identifier: String, sender: Any?) {
+        self.firstTime = false
+    }
 }
 
 extension NovaRefeicaoViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        
-        
+        guard let imgPicked = info[UIImagePickerControllerOriginalImage] as? UIImage else { return }
+        self.refeicaoImage = imgPicked
+        self.textField.text = FoodRecognizerManager.shared.recognizeImage(imgPicked)
+        self.imagePicker.dismiss(animated: true, completion: nil)
     }
     
 }
